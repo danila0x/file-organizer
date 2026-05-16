aa4167078bcdfe55beda8f3adb3d908efa8ecbbe
Danila Potapeshkin <danilapotapeshkin2@gmail.com>
2026-05-16 21:11:12 +0300

    add work with log packet

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -52,4 +53,36 @@ func main() {
 	for key, value := range DefaultRules {
 		fmt.Println(key, value)
 	}
+}
+
+func (fo *FileOrganizer) initLog() error {
+	logFile, err := os.OpenFile(fo.sourceDir, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("Не удалось открыть файл лога %w", err)
+	}
+	fo.logFile = logFile
+	log.SetOutput(logFile)
+	return nil
+}
+
+func (fo *FileOrganizer) logSuccess(message string) {
+	log.Printf("[SUCCESS] %s", message)
+}
+
+func (fo *FileOrganizer) logError(message string) {
+	log.Printf("[ERROR] %s", message)
+}
+
+func (fo *FileOrganizer) Close() error {
+	if fo.logFile != nil {
+		err := fo.logFile.Close()
+		if err != nil {
+			return fmt.Errorf("Ошибка при закрытии лог-файла: %w", err)
+		}
+		fo.logFile = nil
+
+		log.SetOutput(os.Stdout)
+		log.Println("Лог-файл закрыт")
+	}
+	return nil
 }
