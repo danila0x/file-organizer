893724e7686fc070dbe0146b5a11d6f2e1967c9b
Danila Potapeshkin <danilapotapeshkin2@gmail.com>
2026-05-16 19:28:25 +0300

    add map

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	defaultRules := map[string]string{
+		".jpg":  "Images",
+		".jpeg": "Images",
+		".png":  "Images",
+		".pdf":  "Documents",
+		".doc":  "Documents",
+		".docx": "Documents",
+		".txt":  "Documents",
+		".mp3":  "Music",
+		".wav":  "Music",
+		".mp4":  "Video",
+		".avi":  "Video",
+		".zip":  "Archives",
+		".rar":  "Archives",
+	}
+
+	for key, value := range defaultRules {
+		fmt.Println(key, value)
+	}
 }
