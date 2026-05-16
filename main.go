021bccd2cc6cfb2f099c0605e7f1398a5400c163
Danila Potapeshkin <danilapotapeshkin2@gmail.com>
2026-05-16 22:54:45 +0300

    add move file

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 type FileOrganizer struct {
@@ -53,15 +56,33 @@ func main() {
 	for key, value := range DefaultRules {
 		fmt.Println(key, value)
 	}
+
+	org, err := NewFileOrganizer(".")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer org.Close()
+	if err := org.initLog(); err != nil {
+		fmt.Println("Ошибка инициализации лога:", err)
+		return
+	}
+
+	// org.logSuccess("Файл \"report.pdf\" перемещён в директорию \"Documents\"")
+	// org.logError("Невозможно переместить файл \"data.tmp\" - файл занят")
+	// if err := org.moveFile(); err != nil {
+
+	// }
 }
 
 func (fo *FileOrganizer) initLog() error {
-	logFile, err := os.OpenFile(fo.sourceDir, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logPath := filepath.Join(fo.sourceDir, "organizer.log")
+	logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return fmt.Errorf("Не удалось открыть файл лога %w", err)
 	}
 	fo.logFile = logFile
 	log.SetOutput(logFile)
+	log.SetFlags(log.LstdFlags)
 	return nil
 }
 
@@ -83,6 +104,43 @@ func (fo *FileOrganizer) Close() error {
 
 		log.SetOutput(os.Stdout)
 		log.Println("Лог-файл закрыт")
+	}
+	return nil
+}
+
+func (fo *FileOrganizer) moveFile(sourcePath, targetDir string) error {
+	fmt.Printf("sourcePath: %s, targetDir: %s", sourcePath, targetDir)
+	fileName := filepath.Base(sourcePath)
+	fullPath := filepath.Join(fo.sourceDir, targetDir, fileName)
+	fmt.Printf("filename: %s, fullpath: %s", fileName, fullPath)
+	filePath := filepath.Join(fo.sourceDir, targetDir)
+	err := os.MkdirAll(filePath, 0755)
+	if err != nil {
+		fmt.Println("Ошибка создания папки:", err)
+	}
+	fmt.Println("Папка успешно создана")
+	newFilePath := filepath.Join(filePath, fileName)
+	if _, err := os.Stat(newFilePath); err == nil {
+		ext := filepath.Ext(fileName)
+		base := strings.TrimSuffix(fileName, ext)
+		timestamp := time.Now().Format("20060102_150405")
+		newFileName := base + "_" + timestamp + ext
+		targetPath := filepath.Join(filePath, newFileName)
+		if renameErr := os.Rename(sourcePath, targetPath); renameErr != nil {
+			fo.logError(fmt.Sprintf("Не удалось переместить файл %s: %v", fileName, renameErr))
+			return fmt.Errorf("ошибка перемещения файла: %w", renameErr)
+		}
+		fo.logSuccess(fmt.Sprintf("Файл %q перемещён в %q (переименован в %q)", fileName, targetDir, newFileName))
+
+	} else if os.IsNotExist(err) {
+		if renameErr := os.Rename(sourcePath, newFilePath); renameErr != nil {
+			fo.logError(fmt.Sprintf("Не удалось переместить файл %s: %v", fileName, renameErr))
+			return fmt.Errorf("ошибка перемещения файла: %w", renameErr)
+		}
+		fo.logSuccess(fmt.Sprintf("Файл %q перемещён в %q", fileName, targetDir))
+	} else {
+		fo.logError(fmt.Sprintf("Ошибка при проверке файла %s: %v", fileName, err))
+		return fmt.Errorf("ошибка проверки файла: %w", err)
 	}
 	return nil
 }
